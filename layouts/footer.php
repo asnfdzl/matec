d552e083c41330fe95592132a647010b36d6cbb8
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2024-07-02 23:56:30 +0800

    update to php body

---
+++ layouts/footer.php
@@ -0,0 +1,71 @@
+<!-- Section Pre Footer-->
+<section class="section section-lg bg-gray-900">
+    <div class="container">
+        <div class="row row-30">
+            <div class="col-xs-10 col-lg-4 align-self-center">
+                <a class="brand" href="index.html"><img class="brand-logo-light" src="images/logo.png" srcset="images/logo@2x.png 2x" alt="Matec2024"></a>
+            </div>
+            <div class="col-xs-10 col-sm-6 col-lg-4">
+                <h5><span class="big font-weight-sbold" style="color: #c70000;">Event detail</span></h5>
+                <div class="event-detail">
+                    <p class="event-detail-time big" style="color: white;">
+                        <time data-splitting datetime="2024-09-20">September 20 to 22, 2024</time>
+                    </p>
+                    <p class="event-detail-address big" data-splitting style="color: white;">Stadium National Bukit Jalil, <br> Bukit Jalil, Kuala Lumpur</p>
+                    <a class="event-detail-link" href="#"></a>
+                </div>
+                <div class="contact-us">
+                    <h5><span class="big font-weight-sbold" style="color: #c70000;">Contact Us</span></h5>
+                    <div class="event-detail">
+                        <p class="event-detail-time big" style="color: white;">Phone:<br>
+                            En Razi: +6011 - 6648387<br>
+                            En Fizzi: +6010 - 5631270
+                        </p>
+                        <p class="event-detail-time big" style="color: white;">Email:<br>
+                            <a href="mailto:fakharadzi@mara.gov.my" class="event-detail-address big" style="color: white;">fakharadzi@mara.gov.my</a><br>
+                            <a href="mailto:msyafizzi@mara.gov.my" class="event-detail-address big" style="color: white;">msyafizzi@mara.gov.my</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-10 col-sm-6 col-lg-4">
+                <h5><span class="big font-weight-sbold" style="color: #c70000;" >Social Updates</span></h5>
+                <div class="event-detail">
+                    <p class="big" style="color: white;">You may wonder why this event is so popular? If you do,<br class="d-none d-xl-block"> find more info about it here.
+                    </p>
+                    <ul class="list-inline list-inline-xs">
+                        <li data-wow-delay=".2s"><a class="icon icon-rect icon-xs icon-white fa-facebook" href="#" data-triangle=".icon-rect-overlay">
+                            <div class="icon-rect-overlay"></div></a></li>
+                        <li data-wow-delay=".35s"><a class="icon icon-rect icon-xs icon-white fa-twitter" href="#" data-triangle=".icon-rect-overlay">
+                            <div class="icon-rect-overlay"></div></a></li>
+                        <li data-wow-delay=".5s"><a class="icon icon-rect icon-xs icon-white fa-linkedin" href="#" data-triangle=".icon-rect-overlay">
+                            <div class="icon-rect-overlay"></div></a></li>
+                        <li data-wow-delay=".65s"><a class="icon icon-rect icon-xs icon-white fa-youtube-play" href="#" data-triangle=".icon-rect-overlay">
+                            <div class="icon-rect-overlay"></div></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Divider
+<div class="divider divider-gray-900 text-center"></div>-->
+<!-- Footer Classic
+<footer class="section footer-classic context-dark">
+  <div class="container">
+    <p class="rights"><span>Copyright&nbsp;</span><span>&copy;&nbsp;</span><span class="copyright-year"></span><span>&nbsp;</span><span>by</span> <a href="http://bit.ly/dan-fisher">Dan Fisher</a>
+    </p>
+  </div>
+</footer>
+</div>
+<div class="snackbars" id="form-output-global"></div>
+<div class="block-with-svg-gradients">
+<svg xmlns="http://www.w3.org/2000/svg">
+  <defs>
+    <lineargradient id="svg-gradient-primary" x1="0%" y1="100%" x2="100%" y2="0%">
+      <stop offset="0%" style="stop-color:rgb(130,46,168);stop-opacity:1"></stop>
+      <stop offset="100%" style="stop-color:rgb(217,14,144);stop-opacity:1"></stop>
+    </lineargradient>
+  </defs>
+</svg>
+</div>-->
---
+++ layouts/navbar.php
@@ -0,0 +1,47 @@
+    <!-- Section Header Default-->
+    <header class="section page-header">
+        <!--RD Navbar-->
+        <div class="rd-navbar-wrap">
+            <nav class="rd-navbar rd-navbar-classic" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-static" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="46px" data-xl-stick-up-offset="46px" data-xxl-stick-up-offset="76px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
+                <div class="rd-navbar-collapse-toggle rd-navbar-fixed-element-1" data-rd-navbar-toggle=".rd-navbar-collapse"><span></span></div>
+                <div class="rd-navbar-main-outer">
+                    <div class="rd-navbar-main">
+                        <!--RD Navbar Panel-->
+                        <div class="rd-navbar-panel">
+                            <!--RD Navbar Toggle-->
+                            <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                            <!--RD Navbar Brand-->
+                            <div class="rd-navbar-brand">
+                                <!--Brand-->
+                                <a class="brand" href="index.php">
+                                    <img class="brand-logo-dark" src="images/logo.png" srcset="images/logo@2x.png 2x" alt="MATEC" style="width: 600px; height: auto;"/>
+                                    <img class="brand-logo-light" src="images/logo.png" srcset="images/logo@2x.png 2x" alt="matec" style="width: 600px; height: auto;"/>
+                                </a>
+                            </div>
+
+                        </div>
+                        <!-- Rd Navbar Navigation-->
+                        <div class="rd-navbar-main-element">
+                            <div class="rd-navbar-nav-wrap">
+                                <ul class="rd-navbar-nav">
+                                    <li class="rd-nav-item"><a class="rd-nav-link" href="index.php">Home</a>
+                                    </li>
+                                    <!--  <li class="rd-nav-item"><a class="rd-nav-link" href="vendor.html">For Vendor</a>-->
+                                    </li>
+                                    <li class="rd-nav-item"><a class="rd-nav-link" href="event.php">About Event</a>
+                                    </li>
+                                    <li class="rd-nav-item"><a class="rd-nav-link" href="sponsorship.php">Sponsorship</a>
+                                    </li>
+                                    <li class="rd-nav-item"><a class="rd-nav-link" href="contact.php">Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- RD Navbar Collapse-->
+                        <div class="rd-navbar-collapse"><a class="button button-primary" href="#" data-triangle=".button-overlay"><span>Coming Soon</span><span class="button-overlay"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
