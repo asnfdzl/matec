9a9a7df94066bb6e5a2c50425e699261ddf3a267
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2024-07-03 01:40:26 +0800

    update footer

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -4,8 +4,8 @@
         <div class="row row-30">
             <div class="col-xs-10 col-lg-4 align-self-center">
                 <a class="brand" href="/">
-                    <img class="brand-logo-light" src="<?= asset('images/logo.png') ?>" srcset="<?= asset('images/logo@2x.png 2x') ?>" alt="Matec 2024"
-                ></a>
+                    <img class="brand-logo-light" src="<?= asset('images/logo.png') ?>" srcset="<?= asset('images/logo@2x.png 2x') ?>" alt="Matec 2024">
+                </a>
             </div>
             <div class="col-xs-10 col-sm-6 col-lg-4">
                 <h5><span class="big font-weight-sbold" style="color: #c70000;">Event detail</span></h5>
@@ -40,38 +40,53 @@
                     <p class="big" style="color: white;">You may wonder why this event is so popular? If you do,<br class="d-none d-xl-block"> find more info about it here.
                     </p>
                     <ul class="list-inline list-inline-xs">
-                        <li data-wow-delay=".2s"><a class="icon icon-rect icon-xs icon-white fa-facebook" href="#" data-triangle=".icon-rect-overlay">
-                            <div class="icon-rect-overlay"></div></a></li>
-                        <li data-wow-delay=".35s"><a class="icon icon-rect icon-xs icon-white fa-twitter" href="#" data-triangle=".icon-rect-overlay">
-                            <div class="icon-rect-overlay"></div></a></li>
-                        <li data-wow-delay=".5s"><a class="icon icon-rect icon-xs icon-white fa-linkedin" href="#" data-triangle=".icon-rect-overlay">
-                            <div class="icon-rect-overlay"></div></a></li>
-                        <li data-wow-delay=".65s"><a class="icon icon-rect icon-xs icon-white fa-youtube-play" href="#" data-triangle=".icon-rect-overlay">
-                            <div class="icon-rect-overlay"></div></a></li>
+                        <li data-wow-delay=".2s">
+                            <a class="icon icon-rect icon-xs icon-white fa-facebook" href="#" data-triangle=".icon-rect-overlay">
+                                <div class="icon-rect-overlay"></div>
+                            </a>
+                        </li>
+                        <li data-wow-delay=".35s">
+                            <a class="icon icon-rect icon-xs icon-white fa-twitter" href="#" data-triangle=".icon-rect-overlay">
+                                <div class="icon-rect-overlay"></div>
+                            </a>
+                        </li>
+                        <li data-wow-delay=".5s">
+                            <a class="icon icon-rect icon-xs icon-white fa-linkedin" href="#" data-triangle=".icon-rect-overlay">
+                                <div class="icon-rect-overlay"></div>
+                            </a>
+                        </li>
+                        <li data-wow-delay=".65s">
+                            <a class="icon icon-rect icon-xs icon-white fa-youtube-play" href="#" data-triangle=".icon-rect-overlay">
+                                <div class="icon-rect-overlay"></div>
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
         </div>
     </div>
 </section>
-<!-- Divider
-<div class="divider divider-gray-900 text-center"></div>-->
-<!-- Footer Classic
+<!-- Divider-->
+<div class="divider divider-gray-900 text-center"></div>
+
+<!-- Footer Classic-->
 <footer class="section footer-classic context-dark">
-  <div class="container">
-    <p class="rights"><span>Copyright&nbsp;</span><span>&copy;&nbsp;</span><span class="copyright-year"></span><span>&nbsp;</span><span>by</span> <a href="http://bit.ly/dan-fisher">Dan Fisher</a>
-    </p>
-  </div>
+    <div class="container">
+        <p class="rights">
+            <span>&copy; Copyright of MARA AUTOMOTIVE ECO SYSTEM</span>
+            <span class="copyright-year"></span> <br>
+            <span>Powered and designed by </span> <a href="https://www.ardianexus.com/">Ardia Nexus Sdn. Bhd.</a>
+        </p>
+    </div>
 </footer>
-</div>
 <div class="snackbars" id="form-output-global"></div>
 <div class="block-with-svg-gradients">
-<svg xmlns="http://www.w3.org/2000/svg">
-  <defs>
-    <lineargradient id="svg-gradient-primary" x1="0%" y1="100%" x2="100%" y2="0%">
-      <stop offset="0%" style="stop-color:rgb(130,46,168);stop-opacity:1"></stop>
-      <stop offset="100%" style="stop-color:rgb(217,14,144);stop-opacity:1"></stop>
-    </lineargradient>
-  </defs>
-</svg>
-</div>-->
+    <svg xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <lineargradient id="svg-gradient-primary" x1="0%" y1="100%" x2="100%" y2="0%">
+                <stop offset="0%" style="stop-color:rgb(130,46,168);stop-opacity:1"></stop>
+                <stop offset="100%" style="stop-color:rgb(217,14,144);stop-opacity:1"></stop>
+            </lineargradient>
+        </defs>
+    </svg>
+</div>
